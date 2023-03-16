72bbbc05836e89cd3870ca887b18ef88e5ad16a9
Onyekachikelvin <keledan9@gmail.com>
2023-03-16 16:53:12 -0700

    C Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #iclude<stdio.h>
 /**
- * main - A program that prints a line with puts function
+ * main - Entry point
+ *
  * Return:(0)
  */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
