6a9d4d3741e89f73fa9a1f2e1c2c23c937e755d9
Onyekachikelvin <keledan9@gmail.com>
2023-04-12 13:48:35 -0700

    tst

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -19,12 +19,12 @@ int main(void)
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sign(0xff);
+    r = prin_sign(0xff);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sign(-1);
+    r = prin_sign(-1);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
